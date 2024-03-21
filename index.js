6aabe41dfc35c1da6a546bab890bf746369dcd0a
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-21 18:46:24 +0800

    fetched data from pixabay

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const imageRoutes = require('./routes/imageRoutes');
+const imageRoutesUnplash = require('./routes/imageRoutesUnplash');
+const imageRoutesPixabay = require('./routes/imageRoutesPixabay');
 
 const app = express();
 app.use(bodyParser.json());
 
 // Use imageRoutes with the '/api/images' base path
-app.use('/api', imageRoutes); // Corrected the base path
+app.use('/api', imageRoutesUnplash); // Corrected the base path
+app.use('/api', imageRoutesPixabay); // Corrected the base path
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
