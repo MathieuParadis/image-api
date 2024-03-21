c9b272572b973f2cf73496bb4ff10b9a1c2395e2
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-21 18:16:23 +0800

    fetched data from unsplash

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -1,21 +1,20 @@
-// imageRoutes.js
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
 router.get('/images', async (req, res) => {
-    console.log('tes')
   try {
     const unsplashBaseUrl = 'https://api.unsplash.com';
     const unsplashAccessKey = process.env.UNSPLASH_ACCESS_KEY;
 
-    console.log('tes')
-
-    const response = await axios.get(`https://api.unsplash.com/photos/?client_id=wp00NOhUF1dglThw4_zte2Tq3wamjI7Di8BR38Blf68`, {
-    //   params: { client_id: unsplashAccessKey } // Passing API key as a parameter
+    const response = await axios.get(`${unsplashBaseUrl}/photos`, {
+      headers: {
+        Authorization: `Client-ID ${unsplashAccessKey}`
+      }
     });
 
-    const photos = response.data; // Check the structure of the response
+    const photos = response.data;
     res.json(photos);
   } catch (error) {
     console.error('Error fetching photos from Unsplash:', error);
