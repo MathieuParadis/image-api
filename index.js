5b095b4def4c03ef250aab57f086d7fda2990599
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-22 12:02:57 +0800

    combined the requests

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const bodyParser = require('body-parser');
 const imageRoutesUnplash = require('./routes/imageRoutesUnplash');
 const imageRoutesPixabay = require('./routes/imageRoutesPixabay');
 const imageRoutesStoryblocks = require('./routes/imageRoutesStoryblocks');
+const imageRoutes = require('./routes/imageRoutes');
 
 const app = express();
 app.use(bodyParser.json());
 
-// Use imageRoutes with the '/api/images' base path
-app.use('/api', imageRoutesUnplash);
-app.use('/api', imageRoutesPixabay);
+// app.use('/api', imageRoutesUnplash);
+// app.use('/api', imageRoutesPixabay);
 app.use('/api', imageRoutesStoryblocks);
+app.use('/api', imageRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
---
+++ routes/imageRoutes.js
@@ -0,0 +1,100 @@
+require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const crypto = require('crypto');
+
+router.get('/images', async (req, res) => {
+  try {
+    const unsplashBaseUrl = 'https://api.unsplash.com';
+    const pixabayBaseUrl = 'https://pixabay.com/api/?';
+    const storyblocksBaseUrl = 'https://api.graphicstock.com';
+    const unsplashAccessKey = process.env.UNSPLASH_ACCESS_KEY;
+    const pixabayApiKey = process.env.PIXABAY_API_KEY;
+    const storyblocksPublicKey = process.env.STORYBLOCKS_PUBLIC_KEY;
+    const storyblocksPrivateKey = process.env.STORYBLOCKS_PRIVATE_KEY;
+    const storyblocksProjectId = process.env.STORYBLOCKS_PROJECT_ID;
+    const storyblocksUserId = process.env.STORYBLOCKS_USER_ID;
+
+    const unsplashPromise = axios.get(`${unsplashBaseUrl}/search/photos`, {
+      params: { count: 10, query: 'man' },
+      headers: { Authorization: `Client-ID ${unsplashAccessKey}` }
+    });
+
+    const pixabayPromise = axios.get(`${pixabayBaseUrl}key=${pixabayApiKey}`, {
+      params: { q: 'man' }
+    });
+
+    const expires = Math.floor(Date.now() / 1000) + 100;
+    const hmacBuilder = crypto.createHmac('sha256', storyblocksPrivateKey + expires);
+    hmacBuilder.update('/api/v2/images/search');
+    const hmac = hmacBuilder.digest('hex');
+
+    const storyblocksPromise = axios.get(`${storyblocksBaseUrl}/api/v2/images/search`, {
+      params: {
+        APIKEY: storyblocksPublicKey,
+        EXPIRES: expires,
+        HMAC: hmac,
+        project_id: storyblocksProjectId,
+        user_id: storyblocksUserId,
+        keywords: 'men'
+      }
+    });
+
+    let unsplashResponse, pixabayResponse, storyblocksResponse;
+
+    try {
+      unsplashResponse = await unsplashPromise;
+    } catch (error) {
+      console.error('Error while fetching images from Unsplash:', error);
+    }
+
+    try {
+      pixabayResponse = await pixabayPromise;
+    } catch (error) {
+      console.error('Error while fetching images from Pixabay:', error);
+    }
+
+    try {
+      storyblocksResponse = await storyblocksPromise;
+    } catch (error) {
+      console.error('Error while fetching images from Storyblocks:', error);
+    }
+
+    const unsplashImages = (unsplashResponse && unsplashResponse.data.results) ? unsplashResponse.data.results.map(img => ({
+      image_ID: img.id,
+      thumbnails: img.urls.thumb,
+      preview: img.urls.regular,
+      title: img.alt_description,
+      source: 'Unsplash',
+      tags: img.tags.map(tag => tag.title)
+    })) : [];
+
+    const pixabayImages = (pixabayResponse && pixabayResponse.data.hits) ? pixabayResponse.data.hits.map(img => ({
+      image_ID: img.id,
+      thumbnails: img.previewURL,
+      preview: img.largeImageURL,
+      title: null,
+      source: 'Pixabay',
+      tags: img.tags
+    })) : [];
+
+    const storyblocksImages = (storyblocksResponse && storyblocksResponse.data.results) ? storyblocksResponse.data.results.map(img => ({
+      image_ID: img.id,
+      thumbnails: img.thumbnail_url,
+      preview: img.preview_url,
+      title: img.title,
+      source: 'Storyblocks',
+      tags: img.tags
+    })) : [];
+
+    const combinedImages = [...unsplashImages, ...pixabayImages, ...storyblocksImages];
+
+    res.json(combinedImages);
+  } catch (error) {
+    console.error('Error while fetching images:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
